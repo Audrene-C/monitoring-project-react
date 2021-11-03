49ef51ea1d963296bf058193ac4aeb44be862406
Audrene <audrene.coatmeur@gmail.com>
2021-11-03 09:48:10 +0100

    before changing index.js

---
+++ src/components/alerte/Show.js
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { retrieve, reset } from "../../actions/alerte/show";
+import { del } from "../../actions/alerte/delete";
+
+class Show extends Component {
+  static propTypes = {
+    retrieved: PropTypes.object,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string,
+    eventSource: PropTypes.instanceOf(EventSource),
+    retrieve: PropTypes.func.isRequired,
+    reset: PropTypes.func.isRequired,
+    deleteError: PropTypes.string,
+    deleteLoading: PropTypes.bool.isRequired,
+    deleted: PropTypes.object,
+    del: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.retrieve(decodeURIComponent(this.props.match.params.id));
+  }
+
+  componentWillUnmount() {
+    this.props.reset(this.props.eventSource);
+  }
+
+  del = () => {
+    if (window.confirm("Are you sure you want to delete this item?"))
+      this.props.del(this.props.retrieved);
+  };
+
+  render() {
+    if (this.props.deleted) return <Redirect to=".." />;
+
+    const item = this.props.retrieved;
+
+    return (
+      <div>
+        <h1>Show {item && item["@id"]}</h1>
+
+        {this.props.loading && (
+          <div className="alert alert-info" role="status">
+            Loading...
+          </div>
+        )}
+        {this.props.error && (
+          <div className="alert alert-danger" role="alert">
+            <span className="fa fa-exclamation-triangle" aria-hidden="true" />{" "}
+            {this.props.error}
+          </div>
+        )}
+        {this.props.deleteError && (
+          <div className="alert alert-danger" role="alert">
+            <span className="fa fa-exclamation-triangle" aria-hidden="true" />{" "}
+            {this.props.deleteError}
+          </div>
+        )}
+
+        {item && (
+          <table className="table table-responsive table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Field</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">type</th>
+                <td>{item["type"]}</td>
+              </tr>
+              <tr>
+                <th scope="row">date</th>
+                <td>{item["date"]}</td>
+              </tr>
+              <tr>
+                <th scope="row">isQualified</th>
+                <td>{item["isQualified"]}</td>
+              </tr>
+              <tr>
+                <th scope="row">isHealthy</th>
+                <td>{item["isHealthy"]}</td>
+              </tr>
+              <tr>
+                <th scope="row">comment</th>
+                <td>{item["comment"]}</td>
+              </tr>
+              <tr>
+                <th scope="row">site</th>
+                <td>{this.renderLinks("sites", item["site"])}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
+        <Link to=".." className="btn btn-primary">
+          Back to list
+        </Link>
+        {item && (
+          <Link to={`/alertes/edit/${encodeURIComponent(item["@id"])}`}>
+            <button className="btn btn-warning">Edit</button>
+          </Link>
+        )}
+        <button onClick={this.del} className="btn btn-danger">
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  renderLinks = (type, items) => {
+    if (Array.isArray(items)) {
+      return items.map((item, i) => (
+        <div key={i}>{this.renderLinks(type, item)}</div>
+      ));
+    }
+
+    return (
+      <Link to={`../../${type}/show/${encodeURIComponent(items)}`}>
+        {items}
+      </Link>
+    );
+  };
+}
+
+const mapStateToProps = (state) => ({
+  retrieved: state.alerte.show.retrieved,
+  error: state.alerte.show.error,
+  loading: state.alerte.show.loading,
+  eventSource: state.alerte.show.eventSource,
+  deleteError: state.alerte.del.error,
+  deleteLoading: state.alerte.del.loading,
+  deleted: state.alerte.del.deleted,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  retrieve: (id) => dispatch(retrieve(id)),
+  del: (item) => dispatch(del(item)),
+  reset: (eventSource) => dispatch(reset(eventSource)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Show);
